4ad74003f396e39d523ef43de67b8006213de2cd
Petronald Green <petronaldgreen@gmail.com>
2018-02-22 03:10:00 -0400

    Fix assault reference to Player

--- University Life Simulator/Assets/Scripts/Events/Assault.cs
+++ University Life Simulator/Assets/Scripts/Events/Assault.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 using GameFramework.GameObjects;
 
-public class Assault : MonoBehaviour {
-	private GameObject _player;
-	BasePlayer assaulter;
+namespace LifeSimulator
+{
+    public class Assault : MonoBehaviour
+    {
+        private GameObject _player;
+        BasePlayer assaulter;
 
-	// Use this for initialization
-	void Start () {
-		_player = GameObject.Find ("Player");
+        // Use this for initialization
+        void Start()
+        {
+            _player = GameObject.Find("Player");
 
-		// create instance of assaulter
-		BasePlayer assaulter = Instantiate(Resources.Load("Sprites/assaulter/assaulter", typeof(BasePlayer))) as BasePlayer;
-		assaulter.test ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// calculate distance between player and assaulter
-		// print(_player.ToString());
+            // create instance of assaulter
+            BasePlayer assaulter = Instantiate(Resources.Load("Sprites/assaulter/assaulter", typeof(BasePlayer))) as BasePlayer;
+            assaulter.test();
+        }
 
-	}
+        // Update is called once per frame
+        void Update()
+        {
+            // calculate distance between player and assaulter
+            // print(_player.ToString());
+
+        }
+    }
 }
